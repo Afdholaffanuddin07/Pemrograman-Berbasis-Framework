ed6e3fbc3b029fb268db95d0c03cd96a14638493
Afdholaffanuddin07 <afdholganteng07@gmail.com>
2024-03-06 08:43:01 +0700

    Pertemuan 4

--- 03-belajar-komponen/src/utils/utils.tsx
+++ 03-belajar-komponen/src/utils/utils.tsx
@@ -2,7 +2,16 @@ import Image from "next/image";
 
 
 
-export function getImageUrl(imageId: string) {
-    // Gantilah dengan logika atau path sesuai dengan kebutuhan Anda
-    return `https://i.imgur.com/${imageId}.jpg`;
-  }
+// export function getImageUrl(imageId: string) {
+//     // Gantilah dengan logika atau path sesuai dengan kebutuhan Anda
+//     return `https://i.imgur.com/${imageId}.jpg`;
+//   }
+
+//   export function getImageUrlV2(person:{ size:''}) {
+//     return (
+//       'https://i.imgur.com/' +
+//       person.imageId +
+//       size +
+//       '.jpg'
+//     );
+//   }
---
+++ 04-event-dan-state/README.md
@@ -0,0 +1,4 @@
+Praktikum 1
+- Langkah 1
+Membuat element event untuk sebuah button dan juga membuat event handler
+![Output](image/1.png)
